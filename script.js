6917245407995d37b2834556c3ffd2ee47a7dc73
Ganesh2048 <129187187+Ganesh2048@users.noreply.github.com>
2023-12-20 16:45:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 //your JS code here. If required.
 function daysOfAYear(year){
-	if (year%4===0)
+	if(year%4==0)
 	{
-		if (year%100===0) 
-		{
-		    if (year%400==0)
-			{
-		    	return 366;
-		    }
-			else
-				return 365;
-		}
-			else
-				return 366;
-		}
+		if(year%100==0 && year%400!=0)
+			return 365;
+		else
+			return 366;
+	}
 	else
 		return 365;
 		
-	}
 }
